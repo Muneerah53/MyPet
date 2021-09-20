bcdf7f7f46c50e49a3447431744d0edefe0f2612
samarff <samar-alq@hotmail.com>
2021-09-20 09:17:32 +0300

    database final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,324 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '/models/owner.dart';
+import 'models/pet.dart';
+import 'models/global.dart';
 
 
 
-
-void main(){
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
- final Future<FirebaseApp> fbApp =  Firebase.initializeApp();
-   MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> fbApp =  Firebase.initializeApp();
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+        title: 'Flutter Demo',
+        theme: ThemeData(
 
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-             future: fbApp,
-      builder:(context,snapshot) {
-               if (snapshot.hasError){
-                 print("An error has occured ${snapshot.error.toString()}");
-                 return const Text("Something went wrong");}
-      else if (snapshot.hasData) {
-        return Home();
-      }
-      else{return const Center(child:CircularProgressIndicator());}
-      },
-    )
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+          future: fbApp,
+          builder:(context,snapshot) {
+            if (snapshot.hasError){
+              print("An error has occured ${snapshot.error.toString()}");
+              return const Text("Something went wrong");}
+            else if (snapshot.hasData) {
+              return Home();
+            }
+            else{return const Center(child:CircularProgressIndicator());}
+          },
+        )
 
 
     );
   }
 }
-
 class Home extends StatelessWidget {
+
+  String Fname = "unknown";
+  String Lname = "unknown";
+  String Mobile = "unknown";
+  String userID = "unknown";
+  String Email = "unknown";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.red[50],
-
-      body:  Center(
-          child: Column(children: <Widget>[
-
-            Container(
-              margin: EdgeInsets.fromLTRB(50, 40, 50, 30),
-              width: 140,
-              height: 170,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: AssetImage('images/logo.jpeg')
-                  )
-                  )
-                  ),
-            Image.asset(
-              'images/dog2.png',
-              height: 250,
-              width: 350,
+    return Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: Colors.red[50],
+        appBar: AppBar(
+          elevation: 0,
+          brightness: Brightness.light,
+          backgroundColor: Colors.red[50],
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
             ),
-            SizedBox(height: 40),
-            Text("Hi there would you like start a journey in Mypet app ?  " ,textAlign: TextAlign.center, style: new TextStyle(
-              fontSize: 20.0,
-              color: Colors.blueGrey,)),
+          ),
+        ),
+        body:Stack(
+
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    child: CircleAvatar(
+                      radius: 80,
+
+                      backgroundImage: new AssetImage("images/owner.jpg"),
+                    ),
+                  ),
+
+                ],
+
+              ),
+              // Container(
+              // margin: EdgeInsets.only(top: 60,left: 40),
+              // child: CircleAvatar(
+              // radius: 25, backgroundColor: Colors.white,
+              //),
+              //),
+              //Container(
+              // margin: EdgeInsets.only(top: 58,left: 37),
+              //child: CircleAvatar(
+              //radius: 25, backgroundColor: Colors.blueGrey[100],
+              //),
+              //),
+
+
+              //owner container
+              Container(
+                  padding: EdgeInsets.only(top: 220, bottom: 350,left:30),
+                  child:  Container(
+                      padding: EdgeInsets.all(20),
+                      width: 370,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        color: Colors.white,
+
+                      ),
+                      child:
+                      Column(
+                        children: <Widget>[
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 18),
+                            child: FutureBuilder(
+                              future: getOwnerName(),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState != ConnectionState.done)
+                                  return Text("Loading data...Please wait");
+                                return Text("first name:  $Fname",style: petCardTitleStyle);
+                              },
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: FutureBuilder(
+                              future: getOwnerName(),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState != ConnectionState.done)
+                                  return Text("Loading data...Please wait");
+                                return Text("last name:  $Lname",style: petCardTitleStyle);
+                              },
+                            ),
+                          ),Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: FutureBuilder(
+                              future: getOwnerName(),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState != ConnectionState.done)
+                                  return Text("Loading data...Please wait");
+                                return Text("mobile:  $Mobile",style: petCardTitleStyle);
+                              },
+                            ),),
+                          Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child:FutureBuilder(
+                              future: getOwnerName(),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState != ConnectionState.done)
+                                  return Text("Loading data...Please wait");
+                                return Text("email:  $Email",style: petCardTitleStyle);
+                              },
+                            ),
+                          ),
+
+                        ],
+                      ))),
+
+
+              Container(
+                padding: EdgeInsets.only(bottom: 450),
+                child: Center(
+                  child: Text(
+                    'profile information',
+                    style: TextStyle(
+                      fontSize: 25, color: Colors.black87,
+                      fontStyle: FontStyle.italic,),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top:410,left:250),
+
+                width: 120, height:35,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                  color: Colors.black87,
+
+                ),
+
+                child: Row(
+                    children: [ ElevatedButton(onPressed:(){},
+                      child:  Text("Edit", style: new TextStyle(  color: Colors.white),
+                        textAlign: TextAlign.center,),
+
+
+                    ),]
+
+
+                ),),
+              Container(
+                padding: EdgeInsets.only(top: 500, left:30),
+                child: Text(
+                  'My Pets',
+                  style: TextStyle(
+                      fontSize: 30, color: Colors.black87,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.only(top: 550, bottom: 50),
+                child: ListView(
+                  padding: EdgeInsets.only(left: 20),
+                  children: getMyPets(),
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ]));
+  }
+  List<pet> getPets() {
+    List<pet> pets = [];
+    //for (int i = 0; i < 2; i++) {
+    AssetImage profilePic1 = new AssetImage("images/cat.jpeg");
+    pet Pet1 = new pet("cloud", "Cat", profilePic1);
+    pets.add(Pet1);
+    AssetImage profilePic2 = new AssetImage("images/dog.png");
+    pet Pet2 = new pet("Charly", "Dog", profilePic2);
+    pets.add(Pet2);
+    pet Pet3 = new pet("black", "Cat", profilePic1);
+    pets.add(Pet3);
+    // }
+    return pets;
+  }
+
+  List<Widget> getMyPets() {
+    List<pet> techies = getPets();
+    List<Widget> cards = [];
+    for (pet techy in techies) {
+      cards.add(petCard(techy));
+    }
+    return cards;
+  }
 
 
 
-          ])),
-    );
 
+  Map statusStyles = {
+    'Cat' : statusCatStyle,
+    'Dog' : statusDogStyle
+  };
+  Widget petCard(pet newPet) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(right: 20),
+        width: 180,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          color: Colors.white,
+
+        ),
+        child:
+        Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: CircleAvatar(
+                    radius: 45,
+                    backgroundImage: newPet.profilePic,
+                  ),
+                ),
+
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              child: Row(
+                children: <Widget>[
+
+                  Text(newPet.name, style: petCardSubTitleStyle)
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Row(
+                children: <Widget>[
+                  Text(newPet.species, style: statusStyles[newPet.species])
+                ],
+              ),
+            ),
+          ],
+        ));
 
   }
+
+
+  getOwnerName() async {
+
+    var data = await FirebaseFirestore.instance.collection('pet owners').get();
+    int index = 1;
+    Fname = data.docs[index].data()['fname'].toString();
+    Lname = data.docs[index].data()['lname'].toString();
+    Mobile = data.docs[index].data()['mobile'].toString();
+    userID = data.docs[index].data()['uid'].toString();
+
+    data = await FirebaseFirestore.instance.collection('users').get();
+    if(userID==data.docs[index].data()['uid'].toString())
+      Email = data.docs[index].data()['email'].toString();
+  }
+
 }

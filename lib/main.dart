4164c755e63589b4540c4054004fb4be47be41f2
ahadalmutairii <439200907@student.ksu.edu.sa>
2021-09-21 22:33:30 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,44 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 
-
-
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
- final Future<FirebaseApp> fbApp =  Firebase.initializeApp();
-   MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> fbApp =  Firebase.initializeApp();
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-             future: fbApp,
-      builder:(context,snapshot) {
-               if (snapshot.hasError){
-                 print("An error has occured ${snapshot.error.toString()}");
-                 return const Text("Something went wrong");}
-      else if (snapshot.hasData) {
-        return Home();
-      }
-      else{return const Center(child:CircularProgressIndicator());}
-      },
-    )
-
-
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+          future: fbApp,
+          builder:(context,snapshot) {
+            if (snapshot.hasError){
+              print("An error has occured ${snapshot.error.toString()}");
+              return const Text("Something went wrong");}
+            else if (snapshot.hasData) {
+              return Home();
+            }
+            else{return const Center(child:CircularProgressIndicator());}
+          },
+        )
     );
   }
 }
@@ -45,36 +49,11 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.red[50],
-
-      body:  Center(
-          child: Column(children: <Widget>[
-
-            Container(
-              margin: EdgeInsets.fromLTRB(50, 40, 50, 30),
-              width: 140,
-              height: 170,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: AssetImage('images/logo.jpeg')
-                  )
-                  )
-                  ),
-            Image.asset(
-              'images/dog2.png',
-              height: 250,
-              width: 350,
-            ),
-            SizedBox(height: 40),
-            Text("Hi there would you like start a journey in Mypet app ?  " ,textAlign: TextAlign.center, style: new TextStyle(
-              fontSize: 20.0,
-              color: Colors.blueGrey,)),
-
-
-
-          ])),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyPet'),
+        centerTitle: true,
+      ),
     );
-
-
   }
 }

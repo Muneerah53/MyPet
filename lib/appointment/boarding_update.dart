f3b8610b7798699dc306f72e1029db24be46f37e
ahadalmutairii <439200907@student.ksu.edu.sa>
2021-12-10 19:39:40 +0300

    reschedule boarding appointment

--- lib/appointment/boarding_update.dart
+++ lib/appointment/boarding_update.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:MyPet/appointment/appointment_model.dart';
 import 'package:MyPet/appointment_object.dart';
 import 'package:MyPet/models/global.dart';
@@ -24,25 +22,24 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
   int? selectedStartDate;
   String? selectedEndDate;
   String? selectedboardingUID;
-  int isSelected1=0;
-  int isSelected2=0;
-  String? date1,_id, date2;
+  int isSelected1 = 0;
+  int isSelected2 = 0;
+  String? date1, _id, date2;
   appointment a = appointment();
   @override
   void initState() {
     super.initState();
     _id = widget.model.appointmentUID;
+
     date1 = widget.model.date;
     date2 = widget.model.time;
   }
+
   DateTime selected1Date = DateTime.now().add(Duration(days: 1));
   DateTime selected2Date = DateTime.now().add(Duration(days: 2));
 
-
-
   TextEditingController _date1 = new TextEditingController();
   TextEditingController _date2 = new TextEditingController();
-
 
   show1stDialog() async {
     final DateTime? picked = await showDatePicker(
@@ -50,37 +47,31 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
         initialDate: selected1Date,
         firstDate: DateTime.now(),
         lastDate: DateTime(2100)); //from db
-    if (picked != null && picked != selected1Date && (_date2.text.isEmpty || (picked.isBefore(selected1Date)) && !(picked.isAfter(selected1Date)))) {
-
+    if (picked != null &&
+        picked != selected1Date &&
+        (_date2.text.isEmpty ||
+            (picked.isBefore(selected1Date)) &&
+                !(picked.isAfter(selected1Date)))) {
       setState(() {
         selected1Date = picked;
         _date1.value = TextEditingValue(
-            text: DateFormat('EEE, MMM dd yyyy').format(selected1Date)
-        );
-        if(_date2.text.isEmpty){
+            text: DateFormat('EEE, MMM dd yyyy').format(selected1Date));
+        if (_date2.text.isEmpty) {
           setState(() {
             selected2Date = picked.add(Duration(days: 1));
             _date2.value = TextEditingValue(
-                text: DateFormat('EEE, MMM dd yyyy').format(selected2Date)
-            );
-            isSelected2=1;
+                text: DateFormat('EEE, MMM dd yyyy').format(selected2Date));
+            isSelected2 = 1;
           });
-
         }
-
       });
-      isSelected1=1;
-    }
-
-    else{
+      isSelected1 = 1;
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(
-            "drop-off date must be before pick-up date "),
+        content: Text("drop-off date must be before pick-up date "),
         backgroundColor: Theme.of(context).errorColor,
       ));
     }
-
-
   }
 
   show2ndDialog() async {
@@ -89,23 +80,24 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
         initialDate: selected1Date.add(Duration(days: 1)),
         firstDate: DateTime.now(),
         lastDate: DateTime(2100)); //from db
-    if (picked != null && picked != selected2Date && !(picked.isBefore(selected1Date)) && (picked.isAfter(selected1Date))) {
+    if (picked != null &&
+        picked != selected2Date &&
+        !(picked.isBefore(selected1Date)) &&
+        (picked.isAfter(selected1Date))) {
       setState(() {
         selected2Date = picked;
         _date2.value = TextEditingValue(
-            text: DateFormat('EEE, MMM dd yyyy').format(selected2Date)
-        );
+            text: DateFormat('EEE, MMM dd yyyy').format(selected2Date));
       });
-      isSelected2=1;
-    }
-    else {
+      isSelected2 = 1;
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(
-            "pick-up date must be after drop-off date "),
+        content: Text("pick-up date must be after drop-off date "),
         backgroundColor: Theme.of(context).errorColor,
       ));
     }
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: true,
@@ -238,7 +230,6 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
                                         style: BorderStyle.none,
                                       )),
                                 ),
-
                               ),
                             ))),
                     Container(
@@ -256,17 +247,17 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
                         padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
                         child: GestureDetector(
                             onTap: () {
-                              if (isSelected1==0)
+                              if (isSelected1 == 0)
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   SnackBar(
-                                    content: Text("You Must Select Drop-Off Date First"),
+                                    content: Text(
+                                        "You Must Select Drop-Off Date First"),
                                     backgroundColor: Colors.red,
                                   ),
                                 );
                               else
                                 show2ndDialog();
                             },
-
                             child: AbsorbPointer(
                               child: TextField(
                                 controller: _date2,
@@ -322,10 +313,7 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
                                   scrollDirection: Axis.horizontal,
                                   itemCount: snapshot.data!.docs.length,
                                   itemBuilder: (context, index) {
-
-
                                     var t = DateTime.now().hour.toString();
-
 
                                     return OutlinedButton(
                                         style: OutlinedButton.styleFrom(
@@ -334,38 +322,36 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
                                                 color: Colors.transparent)),
                                         onPressed: () {
                                           setState(() {
-                                          //  selectedTimeIndex = index;
-                                         //   selectedTime = stime;
+                                            //  selectedTimeIndex = index;
+                                            //   selectedTime = stime;
                                             selectedboardingUID =
                                                 (snapshot.data!).docs[index].id;
                                           });
                                         },
                                         child: Card(
-                                            //color: selectedTimeIndex == index
-                                             //   ? Color(0XFFFF6B81)
-                                            //    : Colors.white,
+                                          //color: selectedTimeIndex == index
+                                          //   ? Color(0XFFFF6B81)
+                                          //    : Colors.white,
                                             shape: RoundedRectangleBorder(
                                                 borderRadius:
                                                 BorderRadius.circular(
                                                     20.0)),
                                             child: Container(
-                                              padding: EdgeInsets.only(
-                                                  top: 10, bottom: 10),
-                                              margin: EdgeInsets.only(
-                                                  left: 20, right: 20),
-                                              width: 145,
-                                              decoration: BoxDecoration(
-                                                borderRadius:
-                                                BorderRadius.circular(50.0),
-                                              ),
-                                              child: Container(
-                                                child: ListTile(
-
-                                              ),
-                                            ))));
+                                                padding: EdgeInsets.only(
+                                                    top: 10, bottom: 10),
+                                                margin: EdgeInsets.only(
+                                                    left: 20, right: 20),
+                                                width: 145,
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                  BorderRadius.circular(
+                                                      50.0),
+                                                ),
+                                                child: Container(
+                                                  child: ListTile(),
+                                                ))));
                                   });
                             })),
-
                     Center(
                       child: Container(
                         margin: const EdgeInsets.fromLTRB(0, 30, 0, 5),
@@ -373,35 +359,34 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
                         height: 73,
                         child: ElevatedButton(
                             onPressed: () async {
-                              await saveData();
-                              if ((isSelected1==0) ||
-                                  (isSelected2==0)
-                              ) {
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                  content: Text(
-                                      "Please fill out all fields."),
+                              await saveData(
+                                startDate: selected1Date,
+                                endDate: selected2Date,
+                              );
+                              if ((isSelected1 == 0) || (isSelected2 == 0)) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text("Please fill out all fields."),
                                   backgroundColor: Theme.of(context).errorColor,
                                 ));
-                              }
-                              else {
-                                a.time =
-                                    DateFormat('EEE, MMM dd yyyy').format(selected2Date);
-                                a.date =
-                                    DateFormat('EEE, MMM dd yyyy').format(selected1Date);
-
+                              } else {
+                                a.time = DateFormat('EEE, MMM dd yyyy')
+                                    .format(selected2Date);
+                                a.date = DateFormat('EEE, MMM dd yyyy')
+                                    .format(selected1Date);
                               }
                             },
-                            child: const Text('Edit',
-                              style: TextStyle( color:Colors.black,
-                                  fontSize: 18),
+                            child: const Text(
+                              'Edit',
+                              style:
+                              TextStyle(color: Colors.black, fontSize: 18),
                             ),
                             style: ButtonStyle(
                               backgroundColor:
                               MaterialStateProperty.all(Color(0xFFE7F2EC)),
                               shape: MaterialStateProperty.all(
                                   RoundedRectangleBorder(
-                                      borderRadius:
-                                      BorderRadius.circular(12))),
+                                      borderRadius: BorderRadius.circular(12))),
                             )),
                       ),
                     ),
@@ -414,17 +399,17 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
                             onPressed: () {
                               Navigator.of(context).pop();
                             },
-                            child: const Text('Cancel',
-                              style: TextStyle( color:Colors.black,
-                                  fontSize: 18),
+                            child: const Text(
+                              'Cancel',
+                              style:
+                              TextStyle(color: Colors.black, fontSize: 18),
                             ),
                             style: ButtonStyle(
                               backgroundColor:
                               MaterialStateProperty.all(Color(0xFFF3BFBD)),
                               shape: MaterialStateProperty.all(
                                   RoundedRectangleBorder(
-                                      borderRadius:
-                                      BorderRadius.circular(12))),
+                                      borderRadius: BorderRadius.circular(12))),
                             )),
                       ),
                     ),
@@ -451,7 +436,7 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
     }
   }
 
-  saveData() async {
+  saveData({DateTime? startDate, DateTime? endDate}) async {
     if (_id == null) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text("please enter all required information"),
@@ -472,10 +457,20 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
         // "appointmentID": "${widget.model.appointmentUID}"
       });*/
 
+      // Calculate total price
+      final double totalPrice =
+          75.0 * selected2Date.difference(selected1Date).inDays;
+
       await FirebaseFirestore.instance
           .collection('boarding')
-          .doc(_id)
-          .update({"boardingID": _id});
+          .doc(widget.model.appointmentUID)
+          .update(
+        {
+          'endDate': DateFormat('EEE, MMM dd yyyy').format(endDate!),
+          'startDate': DateFormat("EEE, MMM dd yyyy").format(startDate!),
+          'totalPrice': totalPrice,
+        },
+      );
 
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text("appointment has been updated successfully"),
@@ -489,3 +484,4 @@ class _AppointmentUpdateState extends State<Boarding_Update> {
     }
   }
 }
+
